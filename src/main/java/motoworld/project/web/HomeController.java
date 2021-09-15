7522c20e741d799b8e1bb5fcb12472e05a91f641
patrik43210 <67583747+patrik43210@users.noreply.github.com>
2021-09-15 18:36:59 +0100

    update

--- src/main/java/motoworld/project/web/HomeController.java
+++ src/main/java/motoworld/project/web/HomeController.java
@@ -34,7 +34,7 @@ public class HomeController {
         this.logService = logService;
     }
 
-    @GetMapping("/")
+    @GetMapping("moto-world.herokuapp.com")
     public String index(@AuthenticationPrincipal UserDetails principal, Model model) {
         if (principal == null) {
             model.addAttribute("img","images/welcome.jpg");
